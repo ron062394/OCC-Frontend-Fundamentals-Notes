841b176a043e13239bc8f573951a618597cba5ff
AristonCatipay <aristoncatipay123@gmail.com>
2024-06-14 11:33:52 +0800

    Move the deprecated files to a seperate folder.

--- Depracated/task_manager_v1.js
+++ Depracated/task_manager_v1.js
@@ -4,21 +4,18 @@ const todoButton = document.querySelector(".todo-button"); // Button to add new 
 const todoList = document.querySelector(".todo-list"); // UL element to display tasks
 const filterOption = document.querySelector(".filter-todo"); // Dropdown for filtering tasks
 
-// Create event listeners for each button.
-todoButton.addEventListener("click", addTodo); // Step 2: Add new task when the button is clicked
-document.addEventListener("DOMContentLoaded", getTodos); // Step 5: Load tasks from localStorage when the page is loaded
-todoList.addEventListener("click", deleteOrCompleteTodo); // Step 7: Delete or complete a task
-filterOption.addEventListener("change", filterTodo); // Step 10: Filter tasks based on completion status
+// Event Listeners
+todoButton.addEventListener("click", addTodo); // 2. Add new task when the button is clicked
+document.addEventListener("DOMContentLoaded", getTodos); // 5. Load tasks from localStorage when the page is loaded
+todoList.addEventListener("click", deleteOrCompleteTodo); // 7. Delete or complete a task
+filterOption.addEventListener("click", filterTodo); //10. Filter tasks based on completion status
 
-// Step 1: Function to add a new task
+
+
+// 1.Function to add a new task
 function addTodo(e) {
   // Prevent form submission
   e.preventDefault();
-
-  // ****** DO NOT ADD THIS YET ******
-  // Save the task to localStorage
-  saveLocalTodos(todoInput.value);
-  // *********************************
 
   // Create a new todo div
   const todoDiv = document.createElement("div");
@@ -42,45 +39,29 @@ function addTodo(e) {
   trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
   todoDiv.appendChild(trashButton);
 
+  // Save the task to localStorage
+  saveLocalTodos(todoInput.value);
+
   // Append the todo div to the todo list
   todoList.appendChild(todoDiv);
   todoInput.value = "";
 }
 
-/*
-    How to access the local storage
-    1. Right click the browser
-    2. Inpect Element
-    3. Application Tab
-    4. Storage
-    5. Local Storage
-*/
-
-// Step 3: Function to save tasks to localStorage
-// Middleware - a web server is middleware that connects websites to the backend database.
-// Metaphor: You should buy the ingredients first before you start to cook the dish.
-
-// The argument that is passed to the todo parameter is the actual textual data.
+// 3. Function to save tasks to localStorage
 function saveLocalTodos(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-/*
-  // ****** Add this to the addTodo() ******
-
-  // Save the task to localStorage
-  saveLocalTodos(todoInput.value);
-*/
-
-// Step 4: Function to load tasks from localStorage when the page is loaded
+// 4. Function to load tasks from localStorage when the page is loaded
 function getTodos() {
+  // Function to filter tasks based on completion status
   let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
@@ -115,26 +96,21 @@ function getTodos() {
   });
 }
 
-/*  
-    // ****** Add this to the Event Listener Section ******
-    
-    document.addEventListener("DOMContentLoaded", getTodos); // Step 5: Load tasks from localStorage when the page is loaded
-*/
 
-// Step 6: Function to delete or complete task
+// 6. Function to delete or complete task
 function deleteOrCompleteTodo(e) {
   const item = e.target;
 
+  // If the delete button is clicked, remove the task from the list
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
+    console.log(todo);
 
-    // ****** DO NOT ADD THIS YET ******
-    // Delete the task from localStorage
+    //Call remove local todo here
     removeLocalTodos(todo);
-    // *********************************
 
-    todo.addEventListener("transitionend", function () {
+    todo.addEventListener("transitionend", (e) => {
       todo.remove();
     });
   }
@@ -146,33 +122,28 @@ function deleteOrCompleteTodo(e) {
   }
 }
 
-// Step 8: Function to remove the task from the localStorage
-// The argument that will be passed to the todo parameter is the todoDiv container.
+
+// 8. Function to remove the task from the localStorage
 function removeLocalTodos(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-
-  // todo element / div container have 3 children (task, complete button, delete button)
-  // children[0] = "Task content"
-  const task = todo.children[0].innerText;
-  const todoIndex = todos.indexOf(task);
-  todos.splice(todoIndex, 1);
-
-  localStorage.setItem("todos", JSON.stringify(todos));
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+  
+    // todo element / div container have 3 children (task, complete button, delete button)
+    // children[0] = "Task content"
+    const task = todo.children[0].innerText;
+    const todoIndex = todos.indexOf(task);
+    todos.splice(todoIndex, 1);
+  
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
+  
 
-/*  
-    // ****** Add this to the deleteOrCompleteTodo() ******
 
-    // Remove task from localStorage
-    removeLocalTodos(todo);
-*/
-
-// Step 9: Function to filter task based on completion status
+// 9. Function to filter task based on completion status
 function filterTodo(e) {
   const todos = todoList.childNodes;
   todos.forEach(function (todo) {
